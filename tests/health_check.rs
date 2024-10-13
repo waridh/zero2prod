2f77a8c4fbff40ee6dc1fdeede76cdbe5a9b3fc7
waridh <waridh@proton.me>
2024-10-13 18:35:08 -0400

    added http form post request deserialization

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -27,3 +27,48 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(response.content_length(), Some(0));
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let base_addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("http://{}/subscriptions", &base_addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_missing_data() {
+    let base_addr = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = [
+        ("name=le%20guin", "missing email"),
+        ("email=ursula_le_guin%40gmail.com", "missing name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (input, msg) in test_cases {
+        let response = client
+            .post(format!("http://{}/subscriptions", &base_addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(input)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "API did not fail with error code 400. Got {}",
+            msg
+        );
+    }
+}

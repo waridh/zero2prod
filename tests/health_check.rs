ec745214a431cdfccd88d72ce4732840c6ef12ec
waridh <waridh@proton.me>
2024-10-12 21:30:45 -0400

    swapped the testing spawner to returning the full address instead of just the port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-fn spawn_app() -> u16 {
+fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Could not bind application to address");
     let port = listener
         .local_addr()
@@ -9,17 +9,17 @@ fn spawn_app() -> u16 {
     let server = zero2prod::run(listener).expect("Failed to bind to address");
     #[allow(clippy::let_underscore_future)]
     let _ = tokio::spawn(server);
-    port
+    format!("127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    let port = spawn_app();
+    let base_addr = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("http://127.0.0.1:{}/health_check", port))
+        .get(format!("http://{}/health_check", base_addr))
         .send()
         .await
         .expect("Failed to execute request");
